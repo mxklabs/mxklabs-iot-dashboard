18673df04ce603035e0696926391cd9d65bf2a4f
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2018-12-29 12:53:29 +0000

    Added updating graph.

--- app/gui.py
+++ app/gui.py
@@ -1,10 +1,12 @@
-__author__ = 'Dania'
 import matplotlib
-matplotlib.use('TkAgg')
 import numpy as np
+import tkinter
+import random
+
+matplotlib.use('TkAgg')
+
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import tkinter
 
 class Gui(tkinter.Tk):
     def __init__(self, debug, *args, **kwargs):
@@ -19,30 +21,36 @@ class Gui(tkinter.Tk):
         self.size = (800, 480)
         self.geometry("{}x{}".format(*self.size))
         self.box = tkinter.Entry(self)
-        self.button = tkinter.Button(self, text="check", command=self.plot)
-        self.box.pack ()
-        self.button.pack()
-        self.plot()
+        self.button = tkinter.Button(self, text="check", command=self.populate_figure)
+        self.box.place(x=700, y=0, width=100, height=50)
+        self.button.place(x=700, y=50, width=100, height=50)
 
-    def plot (self):
-        x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        v= np.array ([16,16.31925,17.6394,16.003,17.2861,17.3131,19.1259,18.9694,22.0003,22.81226])
-        p= np.array ([16.23697,     17.31653,     17.22094,     17.68631,     17.73641 ,    18.6368,
-            19.32125,     19.31756 ,    21.20247  ,   22.41444   ,  22.11718  ,   22.12453])
+        self.figure = Figure(figsize=(6,4))
+        self.subplot = self.figure.add_subplot(111)
 
-        fig = Figure(figsize=(6,4))
-        a = fig.add_subplot(111)
-        a.scatter(v,x,color='red')
-        a.plot(p, range(2 +max(x)),color='blue')
-        a.invert_yaxis()
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
+        self.canvas.get_tk_widget().place(x=0, y=0, width=700, height=480)
+        self.canvas.draw()
 
-        a.set_title ("Estimation Grid", fontsize=16)
-        a.set_ylabel("Y", fontsize=14)
-        a.set_xlabel("X", fontsize=14)
+    def populate_figure(self):
+        x = np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        v = np.array([random.uniform(10,20) for _ in x])
+        p = np.array([random.uniform(10,20) for _ in x])
 
-        canvas = FigureCanvasTkAgg(fig, master=self)
-        canvas.get_tk_widget().pack()
-        canvas.draw()
+        print("v={}".format(v))
+
+        self.subplot.clear()
+
+        self.subplot.plot(x,v,color='red')
+        #self.subplot.plot(p, range(2 +max(x)),color='blue')
+        #self.subplot.invert_yaxis()
+
+        self.subplot.set_title ("Estimation Grid", fontsize=16)
+        self.subplot.set_ylabel("Y", fontsize=14)
+        self.subplot.set_xlabel("X", fontsize=14)
+
+        self.canvas.draw()
+
 
 gui = Gui(debug=True)
 gui.mainloop()
